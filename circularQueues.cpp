d9d18eb6c8751f53b59eb56a6998b830c6368c77
(faizsltn) <(faizsulthondaudm@gmail.com)>
2025-06-13 08:43:48 +0700

    prosedur iterasi dari awal array hingga REAR

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -84,7 +84,16 @@ public:
             while (FRONT_position <= max -1){
                 cout << queue_array[FRONT_position] << "  ";
                 FRONT_position++;
+
             }
+            FRONT_position = 0;
+
+            //iterasi dari awal array hingga REAR
+            while (FRONT_position <= REAR_position){
+                cout << queue_array[FRONT_position] << "   ";
+                FRONT_position++;
+            }
+            cout << endl;
         }
     }
 };
