7babb2820992678362c77229def0115ef2c3a20c
(faizsltn) <(faizsulthondaudm@gmail.com)>
2025-06-13 08:13:57 +0700

    prosedur mengecek apakah antrian kosong dan else

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -23,5 +23,18 @@ public:
             return;
         }
 
-
+        //cek apakah antrian kosong
+        if (FRONT == -1){
+            FRONT = 0;
+            FRONT = 0;
+        }
+        else{
+            //jika REAR berada di posisi terakhir array , kembali ke awal array
+            if (REAR == max -1)
+                REAR = 0;
+            else    
+                REAR = REAR + 1; 
+        }
+        queue_array[REAR] = num;
+    }
 };
