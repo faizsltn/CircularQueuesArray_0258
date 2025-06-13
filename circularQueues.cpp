38ea8dca301623b28a624323c444292f8a72771a
(faizsltn) <(faizsulthondaudm@gmail.com)>
2025-06-13 08:39:50 +0700

    prosedur else jika  FRONT_position > REAR_position iterasi dari FRONT hingga akhir

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -79,5 +79,12 @@ public:
             }
             cout << endl;
         }
-
+        else {
+            //jika FRONT_position > REAR_position iterasi dari FRONT hingga akhir
+            while (FRONT_position <= max -1){
+                cout << queue_array[FRONT_position] << "  ";
+                FRONT_position++;
+            }
+        }
+    }
 };
